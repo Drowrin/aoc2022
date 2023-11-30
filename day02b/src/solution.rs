394bd1b62023862cc90395d075e85e23edfba2fb
Drowrin <drowrin@gmail.com>
2023-11-30 15:46:36 -0500

    day02b

---
+++ day02b/src/solution.rs
@@ -0,0 +1,85 @@
+use itertools::Itertools;
+
+enum RPS {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+enum Result {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl RPS {
+    fn from(v: &str) -> RPS {
+        match v {
+            "A" => RPS::Rock,
+            "B" => RPS::Paper,
+            "C" => RPS::Scissors,
+            _ => panic!("Invalid input: {v}"),
+        }
+    }
+
+    fn val(self) -> i32 {
+        match self {
+            RPS::Rock => 1,
+            RPS::Paper => 2,
+            RPS::Scissors => 3,
+        }
+    }
+}
+
+impl Result {
+    fn from(v: &str) -> Result {
+        match v {
+            "X" => Result::Lose,
+            "Y" => Result::Draw,
+            "Z" => Result::Win,
+            _ => panic!("Invalid input: {v}"),
+        }
+    }
+
+    fn shape(&self, them: RPS) -> RPS {
+        match self {
+            Result::Lose => match them {
+                RPS::Rock => RPS::Scissors,
+                RPS::Paper => RPS::Rock,
+                RPS::Scissors => RPS::Paper,
+            },
+            Result::Draw => them,
+            Result::Win => match them {
+                RPS::Rock => RPS::Paper,
+                RPS::Paper => RPS::Scissors,
+                RPS::Scissors => RPS::Rock,
+            },
+        }
+    }
+
+    fn val(self) -> i32 {
+        match self {
+            Result::Lose => 0,
+            Result::Draw => 3,
+            Result::Win => 6,
+        }
+    }
+}
+
+fn score(their_shape: &str, result: &str) -> i32 {
+    let them = RPS::from(their_shape);
+    let result = Result::from(result);
+    let me = result.shape(them);
+    me.val() + result.val()
+}
+
+pub fn solution(input: &str) -> String {
+    input
+        .split("\n")
+        .map(|line| {
+            let (their_shape, result) = line.split(" ").collect_tuple().unwrap();
+            score(their_shape, result)
+        })
+        .sum::<i32>()
+        .to_string()
+}
