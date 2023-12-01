1300421e1304675eaddaebcea5d2ac74c090d612
Drowrin <drowrin@gmail.com>
2023-11-30 23:07:36 -0500

    cleanup day02

--- day02a/src/solution.rs
+++ day02a/src/solution.rs
@@ -1,57 +1,62 @@
-enum RPS {
+use itertools::Itertools;
+
+enum Shape {
     Rock,
     Paper,
     Scissors,
 }
 
-impl RPS {
-    fn from(v: &str) -> RPS {
+impl Shape {
+    fn from(v: &str) -> Shape {
         match v {
-            "A" => RPS::Rock,
-            "B" => RPS::Paper,
-            "C" => RPS::Scissors,
-            "X" => RPS::Rock,
-            "Y" => RPS::Paper,
-            "Z" => RPS::Scissors,
+            "A" => Shape::Rock,
+            "B" => Shape::Paper,
+            "C" => Shape::Scissors,
+            "X" => Shape::Rock,
+            "Y" => Shape::Paper,
+            "Z" => Shape::Scissors,
             _ => panic!("Invalid input: {v}"),
         }
     }
 
-    fn score(self, other: RPS) -> i32 {
+    fn val(&self) -> i32 {
         match self {
-            RPS::Rock => {
-                1 + match other {
-                    RPS::Rock => 3,
-                    RPS::Paper => 0,
-                    RPS::Scissors => 6,
-                }
-            }
-            RPS::Paper => {
-                2 + match other {
-                    RPS::Rock => 6,
-                    RPS::Paper => 3,
-                    RPS::Scissors => 0,
-                }
-            }
-            RPS::Scissors => {
-                3 + match other {
-                    RPS::Rock => 0,
-                    RPS::Paper => 6,
-                    RPS::Scissors => 3,
-                }
-            }
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
         }
     }
+}
+
+fn parse_line(line: &str) -> (Shape, Shape) {
+    line.split(" ").map(Shape::from).collect_tuple().unwrap()
+}
+
+fn get_score((them, me): (Shape, Shape)) -> i32 {
+    me.val()
+        + match me {
+            Shape::Rock => match them {
+                Shape::Rock => 3,
+                Shape::Paper => 0,
+                Shape::Scissors => 6,
+            },
+            Shape::Paper => match them {
+                Shape::Rock => 6,
+                Shape::Paper => 3,
+                Shape::Scissors => 0,
+            },
+            Shape::Scissors => match them {
+                Shape::Rock => 0,
+                Shape::Paper => 6,
+                Shape::Scissors => 3,
+            },
+        }
 }
 
 pub fn solution(input: &str) -> impl ToString {
     input
         .split("\n")
-        .map(|line| {
-            let mut shapes = line.split(" ").map(RPS::from);
-            let them = shapes.next().unwrap();
-            let me = shapes.next().unwrap();
-            me.score(them)
-        })
+        .map(parse_line)
+        .map(get_score)
         .sum::<i32>()
 }
--- day02b/src/solution.rs
+++ day02b/src/solution.rs
@@ -1,84 +1,85 @@
-enum RPS {
+use itertools::Itertools;
+
+enum Shape {
     Rock,
     Paper,
     Scissors,
 }
 
-enum Result {
+enum Outcome {
     Lose,
     Draw,
     Win,
 }
 
-impl RPS {
-    fn from(v: &str) -> RPS {
+impl Shape {
+    fn from(v: &str) -> Shape {
         match v {
-            "A" => RPS::Rock,
-            "B" => RPS::Paper,
-            "C" => RPS::Scissors,
+            "A" => Shape::Rock,
+            "B" => Shape::Paper,
+            "C" => Shape::Scissors,
+            _ => panic!("Invalid input: {v}"),
+        }
+    }
+
+    fn val(&self) -> i32 {
+        match self {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
+    }
+}
+
+impl Outcome {
+    fn from(v: &str) -> Outcome {
+        match v {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
             _ => panic!("Invalid input: {v}"),
         }
     }
 
     fn val(self) -> i32 {
         match self {
-            RPS::Rock => 1,
-            RPS::Paper => 2,
-            RPS::Scissors => 3,
+            Outcome::Lose => 0,
+            Outcome::Draw => 3,
+            Outcome::Win => 6,
+        }
+    }
+
+    fn shape(&self, them: Shape) -> Shape {
+        match self {
+            Outcome::Lose => match them {
+                Shape::Rock => Shape::Scissors,
+                Shape::Paper => Shape::Rock,
+                Shape::Scissors => Shape::Paper,
+            },
+            Outcome::Draw => them,
+            Outcome::Win => match them {
+                Shape::Rock => Shape::Paper,
+                Shape::Paper => Shape::Scissors,
+                Shape::Scissors => Shape::Rock,
+            },
         }
     }
 }
 
-impl Result {
-    fn from(v: &str) -> Result {
-        match v {
-            "X" => Result::Lose,
-            "Y" => Result::Draw,
-            "Z" => Result::Win,
-            _ => panic!("Invalid input: {v}"),
-        }
-    }
-
-    fn shape(&self, them: RPS) -> RPS {
-        match self {
-            Result::Lose => match them {
-                RPS::Rock => RPS::Scissors,
-                RPS::Paper => RPS::Rock,
-                RPS::Scissors => RPS::Paper,
-            },
-            Result::Draw => them,
-            Result::Win => match them {
-                RPS::Rock => RPS::Paper,
-                RPS::Paper => RPS::Scissors,
-                RPS::Scissors => RPS::Rock,
-            },
-        }
-    }
-
-    fn val(self) -> i32 {
-        match self {
-            Result::Lose => 0,
-            Result::Draw => 3,
-            Result::Win => 6,
-        }
-    }
+fn parse_line(line: &str) -> (Shape, Outcome) {
+    let (shape, outcome) = line.split(" ").collect_tuple().unwrap();
+    (Shape::from(shape), Outcome::from(outcome))
 }
 
-fn score(their_shape: &str, result: &str) -> i32 {
-    let them = RPS::from(their_shape);
-    let result = Result::from(result);
-    let me = result.shape(them);
-    me.val() + result.val()
+fn get_score((their_shape, outcome): (Shape, Outcome)) -> i32 {
+    let me = outcome.shape(their_shape);
+    me.val() + outcome.val()
 }
 
 pub fn solution(input: &str) -> impl ToString {
     input
         .split("\n")
-        .map(|line| {
-            let mut state = line.split(" ");
-            let their_shape = state.next().unwrap();
-            let result = state.next().unwrap();
-            score(their_shape, result)
-        })
+        .map(parse_line)
+        .map(get_score)
         .sum::<i32>()
 }
