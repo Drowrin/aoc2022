6bab9949e392a176c15b13aa27af79b6ce3a5d66
Drowrin <drowrin@gmail.com>
2023-11-30 23:07:12 -0500

    cleanup day01

--- day01a/src/solution.rs
+++ day01a/src/solution.rs
@@ -1,11 +1,14 @@
+fn get_elf_calories(elf: &str) -> i32 {
+    elf.split("\n")
+        .map(str::parse::<i32>)
+        .map(Result::unwrap)
+        .sum()
+}
+
 pub fn solution(input: &str) -> impl ToString {
     input
         .split("\n\n")
-        .map(|elf| {
-            elf.split("\n")
-                .map(|item| item.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
+        .map(get_elf_calories)
         .max()
         .expect("Empty input!")
 }
