3556ccb7854ee1b146366bd1eb199a42acd9661f
Drowrin <drowrin@gmail.com>
2023-11-30 21:51:28 -0500

    day04b

---
+++ day04b/src/solution.rs
@@ -0,0 +1,26 @@
+use itertools::Itertools;
+
+fn parse_elf(elf: &str) -> (i32, i32) {
+    elf.split("-")
+        .map(str::parse)
+        .map(Result::unwrap)
+        .collect_tuple()
+        .unwrap()
+}
+
+fn parse_line(line: &str) -> ((i32, i32), (i32, i32)) {
+    line.split(",").map(parse_elf).collect_tuple().unwrap()
+}
+
+fn check_overlap((left, right): &((i32, i32), (i32, i32))) -> bool {
+    (left.0 <= right.1 && left.1 >= right.0) || (left.0 <= right.1 && left.1 >= right.0)
+}
+
+pub fn solution(input: &str) -> String {
+    input
+        .split("\n")
+        .map(parse_line)
+        .filter(check_overlap)
+        .count()
+        .to_string()
+}
